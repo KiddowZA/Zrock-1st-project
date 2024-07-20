f520467d8fa5417c61cb879e622df03be5e4d095
KiddowZA <113241607+KiddowZA@users.noreply.github.com>
2024-07-20 13:30:13 +0200

    task1
    this is the first task of adding 2 numbers and making sure it gives the required sum

---
+++ AddTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = scanner.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
